4887a03c055855f0ed3b368e8457d308465b0671
b-aser <asertesfaye@hotmail.com>
2025-09-18 01:44:36 +0300

    Update on HBTN cool

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,9 +3,10 @@ const { argv } = require('node:process');
 if (argv[2] == undefined){
     console.log("No argument");
 }else{
-    argv.forEach((val, index) => {
-        if (index >= 2){
-            console.log(val);
-        }
-    });
+    console.log(argv[2]);
+    // argv.forEach((val, index) => {
+    //     if (index >= 2){
+    //         console.log(argv[2]);
+    //     }
+    // });
 }
