ac1f69ccb2687e69de0059049d8feabd5580c0fb
b-aser <asertesfaye@hotmail.com>
2025-09-18 01:58:04 +0300

    loop to language

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+const langs = ["C", "Pyhton", "JavaScript"];
+const behaviors = ["is fun", "is cool", "is amazing"];
+
+for (let i = 0; i < langs.length; i++) {
+    console.log(langs[i] + " " + behaviors[i]);
+}
